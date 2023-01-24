eb7839e4fa22b7ddff5e8fcc500768dea976a74c
Moaz Mohamed <mo3az.abdellatif@gmail.com>
2023-01-24 02:10:15 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+  /***** Works slider *****/
+  var WorksSwiper = new Swiper(".works-slider .swiper", {
+    // loop: true,
+
+    navigation: {
+      nextEl: ".slider-tools .swiper-button-next",
+      prevEl: ".slider-tools .swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+      767: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        grid: {
+          rows: 2,
+        },
+      },
+    },
+  });
+
+  /***** blog slider *****/
+  var blogSwiper = new Swiper(".blog-slider .swiper", {
+    // loop: true,
+    pagination: {
+      el: ".blog-slider .swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 15,
+      },
+      767: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1500: {
+        slidesPerView: 3,
+        spaceBetween: 48,
+      },
+    },
+  });
+});
